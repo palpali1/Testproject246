516f4d4812072dc99351145a4a4c71432208c811
palpa <palpa@LAPTOP-DOIK9JVK>
2022-03-15 14:01:55 -0400

    first commit

---
+++ src/main/java/pack1/NewTest63c.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest63c {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\palpa\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		 driver.get("https://ui.freecrm.com/");
+  }
+}
